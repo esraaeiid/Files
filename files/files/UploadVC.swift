ce9fded59a06e3750767b7701e72689ebfa9a46c
Esraa Eid <esraa.eiid99@gmail.com>
2020-05-06 14:56:43 +0200

    upload imagefrom gallery

--- files/files/UploadVC.swift
+++ files/files/UploadVC.swift
@@ -8,19 +8,37 @@
 
 import UIKit
 
-class UploadVC: UIViewController {
+class UploadVC: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var ImageSelected: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
+    
     @IBAction func UploadClicked(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = .photoLibrary
+        image.allowsEditing = false
+        self.present(image, animated: true){
+            //After its presented
+        }
     }
     
-
-    
+//image picker handling if its not selected
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.originalImage] as? UIImage{
+            ImageSelected.image = image
+        }
+        else{
+            //error message
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    //TODO:- upload multiple photos
+    //TODO:- upload from cloud storage
+    //TODO:- save uploaded photos
 
 }
